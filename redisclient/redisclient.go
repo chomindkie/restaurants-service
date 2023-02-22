9ae8a75623eb664faf76b9adef8df34cb28b9742
Jintapatee M <mindkie@gmail.com>
2023-02-22 22:30:34 +0700

    Add redis

---
+++ redisclient/redisclient.go
@@ -0,0 +1,72 @@
+package redisclient
+
+import (
+	"encoding/json"
+	"github.com/sirupsen/logrus"
+	"googlemaps.github.io/maps"
+	"time"
+
+	"github.com/go-redis/redis/v7"
+)
+
+type Cache struct {
+	Redis *redis.Client
+}
+
+type Cacher interface {
+	GetRestaurantsByKeyword(key string) *maps.PlacesSearchResponse
+	SaveRestaurantsByKeyword(key string, response maps.PlacesSearchResponse, ttl time.Duration)
+}
+
+func New(redis *redis.Client) *Cache {
+	return &Cache{
+		Redis: redis,
+	}
+}
+
+func NewCache(address string, dbIndex int) *Cache {
+	c := &Cache{}
+	options := redis.Options{
+		Addr: address,
+		DB:   dbIndex,
+	}
+
+	c.Redis = redis.NewClient(&options)
+	_, err := c.Redis.Ping().Result()
+	if err != nil {
+		//acnlog.Fatalf("cannot Ping redis: %s", err.Error())
+		return nil
+	}
+
+	return c
+}
+
+func (c Cache) GetRestaurantsByKeyword(key string) *maps.PlacesSearchResponse {
+	// Create a new person object
+	var response maps.PlacesSearchResponse
+
+	result, err := c.Redis.Get(key).Result()
+	if err != nil {
+		return nil
+	}
+
+	// Unmarshal the JSON string into the PlacesSearchResponse
+	err = json.Unmarshal([]byte(result), &response)
+	if err != nil {
+		return nil
+	}
+
+	logrus.Infof("%s Found on Redis", key)
+	return &response
+}
+
+func (c Cache) SaveRestaurantsByKeyword(key string, response maps.PlacesSearchResponse, ttl time.Duration) {
+	// Convert the PlacesSearchResponse to JSON
+	json, err := json.Marshal(response)
+	if err != nil {
+		panic(err)
+	}
+
+	c.Redis.Set(key, json, ttl)
+	logrus.Infof("%s Save on Redis", key)
+}
